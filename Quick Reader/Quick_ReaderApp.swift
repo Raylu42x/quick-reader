e10e7699694b39fcf793fcf98b63d2d18f61c867
Bennett <bennett@kervian.com>
2026-05-17 14:56:27 -0700

    Initial Commit

---
+++ Quick Reader/Quick_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_ReaderApp.swift
+//  Quick Reader
+//
+//  Created by Bennett Atwood on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Quick_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
